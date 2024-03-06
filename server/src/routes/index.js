21f859742e49fd99938689f053271ceb2dcb3f57
Sharadindu Das <remodas7774@gmail.com>
2024-03-06 21:32:20 +0530

    just minor fixes

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,7 +1,7 @@
-import express from "express"
+import {Router} from "express"
 import { clearTodos, createTodo, deleteTodo, getTodos, updateTodo } from "../controllers/todo.controllers.js"
 
-const router = express.Router()
+const router = Router()
 
 router.get("/get", getTodos)
 router.post("/create", createTodo)
