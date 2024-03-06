b98dd37e908a0882477b54d31319697bf977a495
Sharadindu Das <remodas7774@gmail.com>
2024-03-06 17:18:21 +0530

    changes done

--- server/src/app.js
+++ server/src/app.js
@@ -3,7 +3,7 @@ import cors from "cors"
 
 const app = express()
 
-app.use(cors({ credentials: true, origin: process.env.CORS_ORIGIN }))
+app.use(cors({ credentials: true, origin: ["todo-app-mern-remo.vercel.app"] }))
 app.use(express.json({ limit: "16kb" }))
 app.use(express.urlencoded({ extended: true, limit: "16kb" }))
 
