bf0bce4c77bd0c968135f49a61a7a2befbc0df1d
Sharadindu Das <remodas7774@gmail.com>
2024-03-07 19:27:49 +0530

    changes done

--- server/app.js
+++ server/app.js
@@ -10,6 +10,9 @@ app.use(cors({
 }))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.get("/", (req,res) => {
+    res.send("<h1>Welcome to the Todo App by Remo</h1>")
+})
 app.use("/api", router)
 
 export default app
